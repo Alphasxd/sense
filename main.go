6a80709c115e8f9cdc44972be90d6876879e7aa8
Sheldon <alphasxd@outlook.com>
2024-11-15 17:47:12 +0800

    优化生成测试数据的方式，使用事务处理插入订单记录

--- main.go
+++ main.go
@@ -55,27 +55,46 @@ func createDatabase() (*sql.DB, error) {
 }
 
 func generateTestData(db *sql.DB) error {
-	rand.Seed(time.Now().UnixNano())
+    r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-	// 生成1000个用户id
-	userIDs := make([]int, 1000)
-	for i := 0; i < 1000; i++ {
-		userIDs[i] = i + 1
-	}
+    // 生成1000个用户id
+    userIDs := make([]int, 1000)
+    for i := 0; i < 1000; i++ {
+        userIDs[i] = i + 1
+    }
 
-	// 生成100000条订单记录
-	for i := 0; i < 100000; i++ {
-		uid := userIDs[rand.Intn(len(userIDs))]
-		weight := generateWeight()
-		cost := CalculateShippingCost(weight)
+    // 开始事务
+    tx, err := db.Begin()
+    if err != nil {
+        return err
+    }
 
-		_, err := db.Exec("INSERT INTO orders (uid, weight, cost) VALUES (?, ?, ?)", uid, weight, cost)
-		if err != nil {
-			return err
-		}
-	}
+    stmt, err := tx.Prepare("INSERT INTO orders (uid, weight, cost) VALUES (?, ?, ?)")
+    if err != nil {
+        return err
+    }
+    defer stmt.Close()
 
-	return nil
+    // 生成100000条订单记录
+    for i := 0; i < 100000; i++ {
+        uid := userIDs[r.Intn(len(userIDs))]
+        weight := generateWeight()
+        cost := CalculateShippingCost(weight)
+
+        _, err := stmt.Exec(uid, weight, cost)
+        if err != nil {
+            tx.Rollback()
+            return err
+        }
+    }
+
+    // 提交事务
+    err = tx.Commit()
+    if err != nil {
+        return err
+    }
+
+    return nil
 }
 
 // generateWeight 生成随机重量，保证计费重量的分布权重大致为 1/W

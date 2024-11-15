62f3c13c813173b2876ba80e06ac60bc34bb4f5a
Sheldon <alphasxd@outlook.com>
2024-11-15 18:33:16 +0800

    添加订单结构体并优化生成测试数据和查询订单的逻辑

--- main.go
+++ main.go
@@ -12,6 +12,15 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+// Order 订单结构体
+type Order struct {
+	ID        int
+	UID       int
+	Weight    float64
+	Cost      int
+	CreatedAt string
+}
+
 // CalculateShippingCost 计算快递费用
 func CalculateShippingCost(weight float64) int {
 	chargeableWeight := math.Ceil(weight)
@@ -54,67 +63,67 @@ func createDatabase() (*sql.DB, error) {
 }
 
 func generateTestData(db *sql.DB) error {
-    r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-    // 生成1000个用户id
-    userIDs := make([]int, 1000)
-    for i := 0; i < 1000; i++ {
-        userIDs[i] = i + 1
-    }
+	// 生成1000个用户id
+	userIDs := make([]int, 1000)
+	for i := 0; i < 1000; i++ {
+		userIDs[i] = i + 1
+	}
 
-    // 开始事务
-    tx, err := db.Begin()
-    if err != nil {
-        return err
-    }
+	// 开始事务
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
 
-    stmt, err := tx.Prepare("INSERT INTO orders (uid, weight, cost) VALUES (?, ?, ?)")
-    if err != nil {
-        return err
-    }
-    defer func() {
-        if err := stmt.Close(); err != nil {
-            log.Fatal(err)
-        }
-    }()
+	stmt, err := tx.Prepare("INSERT INTO orders (uid, weight, cost) VALUES (?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := stmt.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
 
-    // 生成100000条订单记录
-    for i := 0; i < 100000; i++ {
-        uid := userIDs[r.Intn(len(userIDs))]
-        weight := generateWeight()
-        cost := CalculateShippingCost(weight)
+	// 生成100000条订单记录
+	for i := 0; i < 100000; i++ {
+		uid := userIDs[r.Intn(len(userIDs))]
+		weight := generateWeight()
+		cost := CalculateShippingCost(weight)
 
-        _, err := stmt.Exec(uid, weight, cost)
-        if err != nil {
-            tx.Rollback()
-            return err
-        }
-    }
+		_, err := stmt.Exec(uid, weight, cost)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
 
-    // 提交事务
-    err = tx.Commit()
-    if err != nil {
-        return err
-    }
+	// 提交事务
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
 
 // generateWeight 生成随机重量，保证计费重量的分布权重大致为 1/W
 func generateWeight() float64 {
 	// 生成一个 0~1 之间的随机数并通过倒数的方式生成重量
-    r := rand.Float64()
-    weight := 1 / math.Pow(r, 2)
+	r := rand.Float64()
+	weight := 1 / math.Pow(r, 2)
 	// 限制最大重量为 100KG
-    if weight > 100 {
-        weight = 100
-    }
+	if weight > 100 {
+		weight = 100
+	}
 	// 保留两位小数
-    return math.Ceil(weight*100) / 100
+	return math.Ceil(weight*100) / 100
 }
 
 func queryOrders(db *sql.DB, uid int) {
-	rows, err := db.Query("SELECT id, weight, cost, created_at FROM orders WHERE uid = ?", uid)
+	rows, err := db.Query("SELECT id, uid, weight, cost, created_at FROM orders WHERE uid = ?", uid)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -123,16 +132,13 @@ func queryOrders(db *sql.DB, uid int) {
 	var totalCost int
 	fmt.Printf("Orders for user %d:\n", uid)
 	for rows.Next() {
-		var id int
-		var weight float64
-		var cost int
-		var createdAt string
-		err := rows.Scan(&id, &weight, &cost, &createdAt)
+		var order Order
+		err := rows.Scan(&order.ID, &order.UID, &order.Weight, &order.Cost, &order.CreatedAt)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("Order ID: %d, Weight: %.2f KG, Cost: %d 元, Created At: %s\n", id, weight, cost, createdAt)
-		totalCost += cost
+		fmt.Printf("Order ID: %d, Weight: %.2f KG, Cost: %d 元, Created At: %s\n", order.ID, order.Weight, order.Cost, order.CreatedAt)
+		totalCost += order.Cost
 	}
 	if err := rows.Err(); err != nil {
 		log.Fatal(err)
